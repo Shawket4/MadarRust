eda22803207deebc730a5439ee1b3ecd95368368
Shawket Ibrahim <shawket.4@icloud.com>
2026-05-21 16:38:45 +0300

    Test Advisor Engine

--- src/menu_advisor/routes.rs
+++ src/menu_advisor/routes.rs
@@ -2,10 +2,12 @@
 
 use actix_web::web;
 use super::handlers::get_report;
+use crate::auth::middleware::JwtMiddleware;
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/menu-advisor")
+            .wrap(JwtMiddleware)
             .route("/report", web::get().to(get_report))
     );
 }
